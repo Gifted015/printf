3145aded60318300fa368a3736124d353196a26d
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-21 15:06:48 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -9,25 +9,29 @@
 
 int _printf(const char *format, ...)
 {
-__attribute__((unused)) int i, x;
+__attribute__((unused)) int i, x, len = 0;
 __attribute__((unused)) char *words;
 va_list str;
 
-va_start (str, format);
+va_start(str, format);
 
 for (i = 0; format[i] != '\0'; i++)
 {
 if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's'))
 {
 if (format[i + 1] == 'c')
+{
 _putchar(va_arg(str, int));
+len++;
+}
 
 else if (format[i + 1] == 's')
 {
 words = va_arg(str, char *);
- for (x = 0; words[x] != '\0'; x++)
+for (x = 0; words[x] != '\0'; x++)
 {
 _putchar(words[x]);
+len++;
 }
 }
 
@@ -36,7 +40,8 @@ i++;
 else
 {
 _putchar(format[i]);
+len++;
 }
 }
-return (i);
+return (len);
 }
