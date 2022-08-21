a7a0bafdc275585bf5ad4c1be5d15ca0f6f2e905
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-21 15:44:08 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -10,38 +10,42 @@
 int _printf(const char *format, ...)
 {
 __attribute__((unused)) int i, x, len = 0;
-__attribute__((unused)) char *words;
+__attribute__((unused)) char *words, chr, empty[] = "(null)";
+if (format != NULL)
+{
 va_list str;
-
 va_start(str, format);
-
 for (i = 0; format[i] != '\0'; i++)
 {
-if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's'))
+if (format[i] == '%')
 {
 if (format[i + 1] == 'c')
 {
-_putchar(va_arg(str, int));
-len++;
-}
-
+chr = va_arg(str, int);
+_putchar(chr), len++, i++; }
 else if (format[i + 1] == 's')
 {
 words = va_arg(str, char *);
+if (words == NULL)
+{
+for (x = 0; empty[x] != '\0'; x++)
+{
+_putchar(empty[x]); } }
+else
+{
 for (x = 0; words[x] != '\0'; x++)
 {
-_putchar(words[x]);
-len++;
+_putchar(words[x]), len++; }
 }
-}
-
-i++;
+i++; }
+else if (format[i + 1] == '%')
+{
+_putchar(format[i]), i++; }
+else
+{
+_putchar(format[i]); }
 }
 else
 {
-_putchar(format[i]);
-len++;
-}
-}
-return (len);
-}
+_putchar(format[i]), len++; } } }
+return (len); }
