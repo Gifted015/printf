7e6be13c8de9f7e1d9bbec439738290afcff53f5
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-21 18:10:44 -0700

    Task 1

--- _printf.c
+++ _printf.c
@@ -7,7 +7,7 @@
  *Return: nothing
  */
 
-int null(int len)
+void null(__attribute__((unused)) int *len)
 {
 int x;
 char empty[] = "(null)";
@@ -15,11 +15,58 @@ char empty[] = "(null)";
 for (x = 0; empty[x] != '\0'; x++)
 {
 _putchar(empty[x]);
-len++;
+*len = (*len) + 1;
+}
 }
 
-return (len);
-}
+
+/**
+ *check - handles specifiers
+ *@len: used to add the lenght of "null" to
+ *the lenght of string originally printed
+ *Return: nothing
+ */
+
+void check(va_list str, const char *format, __attribute__((unused)) int *i, __attribute__((unused)) int *len)
+{
+__attribute__((unused)) int x, in, dec;
+__attribute__((unused)) char *words, chr;
+
+if (format[(*i) + 1] == 'c')
+{
+chr = va_arg(str, int), _putchar(chr); *len = (*len) + 1, *i = (*i) + 1; }
+
+else if (format[(*i) + 1] == 'd')
+{
+dec = va_arg(str, int), print_number(dec); *len = (*len) + 1, *i = (*i) + 1; }
+
+else if (format[(*i) + 1] == 'i')
+{
+in = va_arg(str, int), print_number(in); *len = (*len) + 1, *i = (*i) + 1; }
+
+else if (format[(*i) + 1] == 's')
+{
+words = va_arg(str, char *);
+if (words == NULL)
+{
+null(len); }
+else
+{
+for (x = 0; words[x] != '\0'; x++)
+{
+_putchar(words[x]), *len = (*len) + 1; } }
+*i = (*i) + 1; }
+
+else if (format[(*i) + 1] == '%')
+{
+_putchar(format[(*i)]), *i = (*i) + 1, *len = (*len) + 1; }
+
+else
+{
+if (format[(*i) + 1] == '\0')
+*len = -1;
+else
+_putchar(format[(*i)]), *len = (*len) + 1; } }
 
 
 /**
@@ -31,43 +78,26 @@ return (len);
 
 int _printf(const char *format, ...)
 {
-__attribute__((unused)) int i, x, len = 0;
-__attribute__((unused)) char *words, chr, empty[] = "(null)";
+__attribute__((unused)) int k = 0, j = 0,  *i, *len;
+
+len = &k;
+i = &j;
+
 if (format != NULL)
 {
 va_list str;
 va_start(str, format);
-for (i = 0; format[i] != '\0'; i++)
+for (*i = 0; format[(*i)] != '\0'; *i = (*i) + 1)
 {
-if (format[i] == '%')
+if (format[(*i)] == '%')
 {
-if (format[i + 1] == 'c')
-{
-chr = va_arg(str, int), _putchar(chr), len++, i++; }
-else if (format[i + 1] == 's')
-{
-words = va_arg(str, char *);
-if (words == NULL)
-{
-len = null(len); }
+check(str, format, i, len);
+}
 else
 {
-for (x = 0; words[x] != '\0'; x++)
-{
-_putchar(words[x]), len++; } }
-i++; }
-else if (format[i + 1] == '%')
-{
-_putchar(format[i]), i++, len++; }
-else
-{
-if (format[i + 1] == '\0')
-len = -1;
-else
-_putchar(format[i]), len++; } }
-else
-{
-_putchar(format[i]), len++; } } }
+_putchar(format[(*i)]), *len = (*len) + 1; } }
+va_end(str);
+}
 else
 return (-1);
-return (len); }
+return (*len); }
--- main.h
+++ main.h
@@ -6,5 +6,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+void print_number(int n);
 
 #endif
