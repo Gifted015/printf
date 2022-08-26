30665167cce76ae358eb42a0e5720a74a8c1fd85
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-26 15:20:14 -0700

    Task 5

--- check3.c
+++ check3.c
@@ -10,13 +10,11 @@
 
 int wordhex(va_list str, int *i, int *len)
 {
-unsigned int a, b, c, d, x, *bin = NULL;
-char upp[] = "0123456789ABCDEF", *words, nonprint[] = "\\x0";
+unsigned int a, b, c, d = 3, x, *bin = NULL;
+char upp[] = "0123456789ABCDEF", *words, nonprint[5] = "\\x0";
 words = va_arg(str, char *);
 if (words == NULL)
-{
 null(len);
-}
 else
 {
 for (x = 0; words[x] != '\0'; x++)
@@ -26,31 +24,33 @@ if ((words[x] > 0 && words[x] < 32) || words[x] >= 127)
 for (c = words[x], a = 1; c > 0; c = (c / 16), a++)
 {
 bin = realloc(bin, (sizeof(int) * (a + 1)));
-bin[a] = c % 16;
-}
-for (c = a - 1; c >= 1; c--)
+bin[a] = c % 16; }
+for (c = a - 1; c > 0; c--)
 {
-for (b = 0; upp[b] != '\0'; b++)
+nonprint[2] = '0';
+for (b = 0; upp[b] != 0; b++)
 {
 if (bin[c] == b)
 {
-for (d = 0; nonprint[d] != '\0'; d++)
-_putchar(nonprint[d]), *len = (*len) + 1;
-_putchar(upp[b]);
-*len = (*len) + 1;
+nonprint[d] = upp[b];
+if (a == 3)
+d = 4;
 }
 }
 }
-}
-else
+if (d == 4)
 {
+nonprint[2] = nonprint[3];
+nonprint[3] = nonprint[4], nonprint[4] = '\0'; }
+for (b = 0; b <= 3; b++)
+_putchar(nonprint[b]), *len = (*len) + 1;
+d = 3; }
+else
 _putchar(words[x]), *len = (*len) + 1;
 }
 }
-}
 *i = (*i) + 1;
-return (*i);
-}
+return (*i); }
 
 
 /**
--- main.c
+++ main.c
@@ -1,12 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: 0 on success, error code otherwise
  */
 int main(void)
 {
-    _printf("%S\n", "Best\nSchool");
-    return (0);
+	int len, len2;
+
+	len = _printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
+	len2 = printf("Could you print some non-prntable characters?\nSure:\\x1F\\x7F\\x0A\nThanks!\n");
+	printf("len: %d\tlen2: %d\n", len, len2);
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
 }
